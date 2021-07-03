ffdd39ccfba446882f70612c31d6fb9ce8408432
Nathan Drake <nathandrakedrums@gmail.com>
2021-07-03 16:47:17 -0500

    Added local host

--- sleeper_portfolio/settings.py
+++ sleeper_portfolio/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dynasty-ninja.herokuapp.com']
+ALLOWED_HOSTS = [
+    'dynasty-ninja.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
