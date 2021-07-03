d7220105646a6c27eeec2c7148444876fd092fee
Nathan Drake <nathandrakedrums@gmail.com>
2021-07-03 16:51:22 -0500

    django-heroku

--- sleeper_portfolio/settings.py
+++ sleeper_portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +144,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
+django_heroku.settings(locals())
