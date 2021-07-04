aa49e81f165bfb59f69803a7f06de91a0ea820c3
Nathan Drake <nathandrakedrums@gmail.com>
2021-07-03 20:57:52 -0500

    debug false

--- sleeper_portfolio/settings.py
+++ sleeper_portfolio/settings.py
@@ -26,7 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = False
+
+#(os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = [
     'dynasty-ninja.herokuapp.com',
