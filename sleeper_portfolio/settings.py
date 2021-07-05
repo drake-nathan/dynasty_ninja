ad77fc356288698dc236066c7b03b14eb8b453fe
Nathan Drake <nathandrakedrums@gmail.com>
2021-07-05 10:30:23 -0500

    fix debug

--- sleeper_portfolio/settings.py
+++ sleeper_portfolio/settings.py
@@ -14,9 +14,10 @@ from pathlib import Path
 import os
 import django_heroku
 
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
